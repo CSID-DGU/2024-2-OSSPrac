3ba5bfa80374e11eb98710a4ceeafa3e4eb80d82
Seungwoo-JI64 <swoo64@dgu.ac.kr>
2024-11-07 14:25:11 +0900

    기본 베이스 자료 업로드

---
+++ Subject3_1/ex4.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+@app.route('/')
+def input():
+    return render_template('input.html')
+
+@app.route('/result',methods=['POST','GET'])
+def result():
+    if request.method =='POST':
+       result=dict()
+       result['Name']=request.form.get('name')
+       result['StudentNumber']=request.form.get('StudentNumber')
+       result['languages'] = request.form.getlist('languages')
+       result['languages'] =  ','.join(result['languages'])
+       return render_template('result.html',result=result)
+
+if __name__ =='__main__':
+     app.run(debug=True)
